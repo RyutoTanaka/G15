44bd22ab9c5e89a01771bcde3409fd5bc0ae8376
RyutoTanaka <ryuto.tanaka314@gmail.com>
2024-05-23 15:46:45 +0900

    fix spell miss

--- Jetson/src/driver/setup.py
+++ Jetson/src/driver/setup.py
@@ -5,7 +5,7 @@ package_name = 'driver'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'motor_driver = driber.motor_driver:main',
+            'motor_driver = driver.motor_driver:main',
         ],
     },
 )
