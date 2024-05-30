b3e9d72801489e7e0064e93adee104e4037ca022
RyutoTanaka <ryuto.tanaka314@gmail.com>
2024-05-30 12:19:23 +0900

    motor_driver でPWMを出力できるように修正

--- Jetson/src/driver/driver/motor_driver.py
+++ Jetson/src/driver/driver/motor_driver.py
@@ -5,22 +5,14 @@ from rclpy.node import Node
 from rclpy import qos
 from std_msgs.msg import Float32
 
-import RPi.GPIO as GPIO
+import Jetson.GPIO as GPIO
 import math
 
 class MotorDriver(Node):
     PWM_RIGHT_PIN = 32
     PWM_LEFT_PIN = 33
     DIRECTION_RIGHT_PIN = 31
-    DIRECTION_LEFT_PIN = 29
-
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(PWM_RIGHT_PIN, GPIO.OUT, initial=GPIO.HIGH)
-    right_pwm = GPIO.PWM(PWM_RIGHT_PIN, 1000)
-    GPIO.setup(PWM_LEFT_PIN, GPIO.OUT, initial=GPIO.HIGH)
-    left_pwm = GPIO.PWM(PWM_LEFT_PIN, 1000)
-    GPIO.setup(DIRECTION_RIGHT_PIN, GPIO.OUT)
-    GPIO.setup(DIRECTION_LEFT_PIN, GPIO.OUT)
+    DIRECTION_LEFT_PIN = 15
     def __init__(self):
         super().__init__('motor_driber')
         
@@ -30,35 +22,46 @@ class MotorDriver(Node):
             'pwm_right',
             self.pwm_right_callback,
             qos.qos_profile_sensor_data)
-        self.subscription  # prevent unused variable warning
-        
+        self.pwm_right_subscription  # prevent unused variable warning
+                
         # pwm_left (-1.0 ~ 1.0)
         self.pwm_left_subscription = self.create_subscription(
             Float32,
             'pwm_left',
             self.pwm_left_callback,
             qos.qos_profile_sensor_data)
-        self.subscription  # prevent unused variable warning
+        self.pwm_left_subscription  # prevent unused variable warning
 
-    def __del__(self):
-        right_pwm.stop()
-        left_pwm.stop()
-        GPIO.cleanup()
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.PWM_RIGHT_PIN, GPIO.OUT, initial=GPIO.HIGH)
+        self.right_pwm = GPIO.PWM(self.PWM_RIGHT_PIN, 1000)
+        GPIO.setup(self.PWM_LEFT_PIN, GPIO.OUT, initial=GPIO.HIGH)
+        self.left_pwm = GPIO.PWM(self.PWM_LEFT_PIN, 1000)
+        GPIO.setup(self.DIRECTION_RIGHT_PIN, GPIO.OUT)
+        GPIO.setup(self.DIRECTION_LEFT_PIN, GPIO.OUT)
+    
+    def timer_callback(self):
+        if(timeout_cnt > 100):
+            self.right_pwm.stop()
+            self.left_pwm.stop()
+        self.timeout_cnt += 1
     
     def pwm_right_callback(self, msg):
+        self.get_logger().info('I heard right: "%f"' % msg.data)
         if msg.data >= 0.0 :
-            GPIO.output(DIRECTION_RIGHT_PIN, GPIO.LOW)
+            GPIO.output(self.DIRECTION_RIGHT_PIN, GPIO.LOW)
         else :
-            GPIO.output(DIRECTION_RIGHT_PIN, GPIO.HIGH)
-        right_pwm.start(math.fabs(msg.data))
+            GPIO.output(self.DIRECTION_RIGHT_PIN, GPIO.HIGH)
+        self.right_pwm.start(math.fabs(msg.data))
 
 
     def pwm_left_callback(self, msg):
-        if msg.data >= 0.0 :
-            GPIO.output(DIRECTION_LEFT_PIN, GPIO.HIGH)
+        self.get_logger().info('I heard left: "%f"' % msg.data)
+        if msg.data > 0.0 :
+            GPIO.output(self.DIRECTION_LEFT_PIN, GPIO.HIGH)
         else :
-            GPIO.output(DIRECTION_LEFT_PIN, GPIO.LOW)
-        right_pwm.start(math.fabs(msg.data))
+            GPIO.output(self.DIRECTION_LEFT_PIN, GPIO.LOW)
+        self.left_pwm.start(math.fabs(msg.data))
 
 
 def main(args=None):
