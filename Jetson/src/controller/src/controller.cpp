2459f57e183df5c200158b05a7d73ab14cb9f861
RyutoTanaka <ryuto.tanaka@outlook.jp>
2024-08-13 17:11:21 +0900

    モーター制御をSTM32で行うように変更

---
+++ Jetson/src/controller/src/controller.cpp
@@ -0,0 +1,64 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include "controller/spi.hpp"
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+    MinimalPublisher()
+    : Node("minimal_publisher"), count_(0),spi_("/dev/spidev0.0")
+    {
+      spi_.set_max_speed_hz(1000000);
+      spi_.set_bits_per_word(8);
+      spi_.set_mode(0);
+      spi_.set_lsb(0);
+
+      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      static auto spi_data = [&]() {
+        kiks::io::spi::Data spi_data = {};
+        spi_data.len = sizeof(extension_tx_data_);
+        spi_data.speed_hz = 1000000;
+        spi_data.bits_per_word = 8;
+        spi_data.tx_buf = reinterpret_cast<std::uint64_t>(&extension_tx_data_);
+        spi_data.rx_buf = reinterpret_cast<std::uint64_t>(&extension_rx_data);
+        return spi_data;
+      }();
+      spi_.write(&spi_data);
+
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+    
+    kiks::io::spi::Master spi_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
